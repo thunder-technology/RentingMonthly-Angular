3feea2579a1aba0bc6389249032db1c566ae1fe5
plutowang <wbc.pluto@gmail.com>
2019-06-20 23:24:33 -0400

    add house-recording and owner-recording components init and change icon

---
+++ src/app/house-recording/house-recording.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-house-recording',
+  templateUrl: './house-recording.component.html',
+  styleUrls: ['./house-recording.component.scss']
+})
+export class HouseRecordingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
